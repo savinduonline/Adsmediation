33c371ebc71b153a79602d048f0691a12198232d
savinduonline <savinduen@gmail.com>
2022-06-11 11:41:42 +0530

    Commit

--- app/src/main/java/lk/savindu/test/adsmediation/MainActivity.java
+++ app/src/main/java/lk/savindu/test/adsmediation/MainActivity.java
@@ -4,18 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import lk.savindu.mediationads.MediationAds;
-
 public class MainActivity extends AppCompatActivity {
-
-    MediationAds mediationAds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mediationAds = new MediationAds(MainActivity.this);
 
 
     }
